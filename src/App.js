7247d66479b75b4a5226c7c121df2fa98c270598
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2021-09-09 18:58:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// import { useState } from "react";
-// import "./App.css";
-// import Header from "./Header";
-// import Nav from "./Nav";
-// import Results from "./Results";
-// import requests from "./requests";
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Results from "./Results";
+import requests from "./requests";
 
-// function App() {
-//   const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
-//   return (
-//     <div className="App">
-//       <Header />
-//       <Nav setSelectedOption={setSelectedOption} />
-//       <Results selectedOption={selectedOption} />
-//     </div>
-//   );
-// }
+function App() {
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+  return (
+    <div className="App">
+      <Header />
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
